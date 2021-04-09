0997f47f531b5ead9a7d35394babfb70f83269bd
JC Castro <77168499+jccastro13@users.noreply.github.com>
2021-04-09 17:44:32 -0600

    Add files via upload

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,23 +18,31 @@ with open(os.path.join("Resources", "election_data.csv"),'r') as csvfile:
             candidate_list[candidate] += 1
         else:
             candidate_list[candidate]= 1
+with open(os.path.join("analysis", "analysis.txt"),'w') as poll_analysis:
 
-print("Election Results")
-print("--------------------")
-print("Total Votes: " + str(voter_count))
-print("--------------------")
+    print("Election Results")
+    poll_analysis.write("Election Results\n")
+    print("--------------------")
+    poll_analysis.write("---------------\n")
+    print("Total Votes: " + str(voter_count))
+    poll_analysis.write("Total Votes: "+str(voter_count)+"\n")
+    print("--------------------")
+    poll_analysis.write("---------------\n")
 
-max_candidate_votes =0
+    max_candidate_votes =0
 
-for candidate in candidate_list:
-    print(candidate + "("+ str(candidate_list[candidate])+")")
-    print(round(float(candidate_list[candidate]/voter_count)*100,4))
+    for candidate in candidate_list:
+        percentage=round(float(candidate_list[candidate]/voter_count)*100,4)
+        print(f"{candidate}: {percentage}% ({candidate_list[candidate]})")
+        poll_analysis.write(f"{candidate}: {percentage}% ({candidate_list[candidate]})\n")
 
-    if candidate_list[candidate] > max_candidate_votes:
-        max_candidate_votes = candidate_list[candidate]
-for candidate in candidate_list:
-    if candidate_list[candidate]==max_candidate_votes:
-        print("_________________")
-        print("Winner: "+ str(candidate))
-        print("-----------------")
-
+        if candidate_list[candidate] > max_candidate_votes:
+            max_candidate_votes = candidate_list[candidate]
+    for candidate in candidate_list:
+        if candidate_list[candidate]==max_candidate_votes:
+            print("_________________")
+            poll_analysis.write("----------------\n")
+            print("Winner: "+ str(candidate))
+            poll_analysis.write("Winner: "+ str(candidate)+"\n")
+            print("-----------------")
+            poll_analysis.write("----------------\n")
